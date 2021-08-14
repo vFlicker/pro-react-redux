5dffc3a28ec8a79b84b88cace0076f00cc6ceff1
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-14 22:21:29 +0300

    Fix. Migrate networking from constructor to componentDidMount

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -13,9 +13,9 @@ export default class RandomPlanet extends Component {
     error: false,
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    this.intervalId = setInterval(this.updatePlanet, 6000);
   }
 
   onPlanetLoaded = (planet) => {
@@ -33,7 +33,7 @@ export default class RandomPlanet extends Component {
     });
   }
 
-  updatePlanet()  {
+  updatePlanet = () => {
     const id = Math.floor(Math.random() * 20) + 1;
 
     this.api
@@ -68,22 +68,22 @@ const PlanetView = ({ planet }) => {
       <img
         className="planet-image"
         src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        alt={ name }
+        alt={name}
       />
       <div>
-        <h4>{ name }</h4>
+        <h4>{name}</h4>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
             <span className="term">Population</span>
-            <span>{ population }</span>
+            <span>{population}</span>
           </li>
           <li className="list-group-item">
             <span className="term">Rotation Period</span>
-            <span>{ rotationPeriod }</span>
+            <span>{rotationPeriod}</span>
           </li>
           <li className="list-group-item">
             <span className="term">Diameter</span>
-            <span>{ diameter }</span>
+            <span>{diameter}</span>
           </li>
         </ul>
       </div>
