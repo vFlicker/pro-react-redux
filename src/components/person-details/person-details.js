b864cf4c47a4a9b15eed1dd0b6b8cb18970e63b4
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-15 16:32:08 +0300

    Add person-details component

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,33 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Spinner from '../spinner';
+import ErrorButton from '../error-button/error-button';
+import Api from '../../api';
 import './person-details.css';
 
-const PersonDetails = () => {
+export default class PersonDetails extends Component {
+  api = new Api();
+
+  state = {
+    person: null,
+    loaded: true,
+  }
+
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      this.setState({ loaded: true })
+      this.updatePerson();
+    }
+  }
+
+  onPersonLoaded = (person) => {
+    this.setState({
+      person,
+      loaded: false,
+    });
+  }
+
+  updatePerson() {
+    const { personId } = this.props;
+
+    if (!personId) {
+      return;
+    }
+
+    this.api
+      .getPerson(personId)
+      .then(this.onPersonLoaded);
+  }
+
+  render() {
+    if (!this.state.person) {
+      return <span>Select a person from a list</span>;
+    }
+
+    const spinner = this.state.loaded ? <Spinner /> : null;
+    const person = !this.state.loaded ? personItem(this.state.person) : null;
+
+    return (
+      <div className="person-details card">
+        {spinner}
+        {person}
+      </div>
+    );
+  }
+}
+
+const personItem = ({ id, name, gender, birthYear, eyeColor }) => {
   return (
-    <div className="person-details card">
+    <React.Fragment>
       <img
         className="person-image"
-        src="https://starwars-visualguide.com/assets/img/characters/3.jpg"
-        alt="Planet name"
+        src={ `https://starwars-visualguide.com/assets/img/characters/${id}.jpg` }
+        alt={ name }
       />
       <div className="card-body">
-        <h4>R2-D2</h4>
+        <h4>{ name }</h4>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
             <span className="term">Gender</span>
-            <span>male</span>
+            <span>{ gender }</span>
           </li>
           <li className="list-group-item">
             <span className="term">Birth Year</span>
-            <span>43</span>
+            <span>{ birthYear }</span>
           </li>
           <li className="list-group-item">
             <span className="term">Eye Color</span>
-            <span>red</span>
+            <span>{ eyeColor }</span>
           </li>
         </ul>
+        <ErrorButton />
       </div>
-    </div>
-  )
-}
-
-export default PersonDetails;
+    </React.Fragment>
+  );
+};
