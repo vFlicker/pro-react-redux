43ec918a0fa82a51c8987306814f20efc2f9d06a
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-26 23:14:53 +0300

    Add action booksRequested

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,13 +3,15 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import BookItem from '../book-item';
 import withBookstoreService from '../hoc';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, booksRequested } from '../../actions';
 import './book-list.css';
 import Spinner from '../spiner';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService, booksLoaded } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+
+    booksRequested();
 
     bookstoreService.getBooks()
       .then((data) => booksLoaded(data));
@@ -44,6 +46,7 @@ const mapStateToProps = ({ books, loading }) => {
 
 const mapDispatchToProps = {
   booksLoaded,
+  booksRequested,
 };
 
 export default compose(
