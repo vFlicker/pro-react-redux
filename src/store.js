ece53f224564cd4da487892379389fee102fe95a
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-30 15:43:40 +0300

    Add store enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,38 @@
 import { createStore } from 'redux';
+import { compose } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logEnhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+
+  const originalDispatch = store.dispatch;
+
+  store.dispatch = (action) => {
+    console.log(action.type)
+    return originalDispatch(action);
+  };
+
+  return store;
+};
+
+const stringEnhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+
+  const originalDispatch = store.dispatch;
+
+  store.dispatch = (action) => {
+    if (typeof action === 'string') {
+      return originalDispatch({type: action});
+    }
+
+    return originalDispatch(action);
+  };
+
+  return store;
+};
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+store.dispatch('FOO_BAR');
 
 export default store;
