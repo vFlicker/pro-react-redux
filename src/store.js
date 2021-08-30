c17ab320d2150481eb86b1f46147dca7f2d9fbd1
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-30 16:09:33 +0300

    Add Middlewares, remove Enhancers

--- src/store.js
+++ src/store.js
@@ -1,37 +1,20 @@
-import { createStore } from 'redux';
-import { compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const logEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-
-  const originalDispatch = store.dispatch;
-
-  store.dispatch = (action) => {
-    console.log(action.type)
-    return originalDispatch(action);
-  };
-
-  return store;
+const logMiddleware = ({ getState }) => (next) => (action) => {
+  console.log(action.type, getState())
+  return next(action);
 };
 
-const stringEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({ type: action });
+  }
 
-  const originalDispatch = store.dispatch;
-
-  store.dispatch = (action) => {
-    if (typeof action === 'string') {
-      return originalDispatch({type: action});
-    }
-
-    return originalDispatch(action);
-  };
-
-  return store;
+  return next(action);
 };
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 
 store.dispatch('FOO_BAR');
 
