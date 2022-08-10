a217e5908e82cacc62c7a700718d986979cc5bb4
Vladislav <flickervladislav@gmail.com>
2022-08-10 23:41:35 +0300

    refactor: services

--- src/components/hoc-halper/with-data.js
+++ src/components/hoc-halper/with-data.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Spinner from '../spinner';
+
+import { Spinner } from '../spinner';
 import { ErrorIndicator } from '../error-indicator';
 
 const withData = (View) => {
--- src/services/api.js
+++ src/services/api.js
@@ -1,95 +1,83 @@
 export class Api {
-  _apiBase = 'https://swapi.dev/api';
-  _imageBase = 'https://starwars-visualguide.com/assets/img';
+  #apiBase = 'https://swapi.dev/api';
+  #imageBase = 'https://starwars-visualguide.com/assets/img';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
+  getAllPeople = async () => {
+    const res = await this.#getResource(`/people/`);
+    return res.results.map(this.#transformPerson);
+  };
+
+  getAllPlanets = async () => {
+    const res = await this.#getResource(`/planets/`);
+    return res.results.map(this.#transformPlanet);
+  };
+
+  getAllStarships = async () => {
+    const res = await this.#getResource(`/starships/`);
+    return res.results.map(this.#transformStarship);
+  };
+
+  getPerson = async (id) => {
+    const person = await this.#getResource(`/people/${id}/`);
+    return this.#transformPerson(person);
+  };
+
+  getPlanet = async (id) => {
+    const planet = await this.#getResource(`/planets/${id}/`);
+    return this.#transformPlanet(planet);
+  };
+
+  getStarship = async (id) => {
+    const starship = await this.#getResource(`/starships/${id}/`)
+    return this.#transformStarship(starship);
+  };
+
+  getPersonImage = (id) => `${this.#imageBase}/characters/${id}.jpg`;
+
+  getPlanetImage = (id) => `${this.#imageBase}/planets/${id}.jpg`;
+
+  getStarshipImage = (id) => `${this.#imageBase}/starships/${id}.jpg`;
+
+  #getResource = async (url) => {
+    const res = await fetch(`${this.#apiBase}${url}`);
 
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, received ${res.status}`);
     }
 
     return await res.json();
-  }
+  };
 
-  getAllPeople = async () => {
-    const res = await this.getResource(`/people/`);
-    return res.results.map(this._transformPerson);
-  }
-
-  getAllPlanets = async () => {
-    const res = await this.getResource(`/planets/`);
-    return res.results.map(this._transformPlanet);
-  }
-
-  getAllStarships = async () => {
-    const res = await this.getResource(`/starships/`);
-    return res.results.map(this._transformStarship);
-  }
-
-  getPerson = async (id) => {
-    const person = await this.getResource(`/people/${id}/`);
-    return this._transformPerson(person);
-  }
-
-  getPlanet = async (id) => {
-    const planet = await this.getResource(`/planets/${id}/`);
-    return this._transformPlanet(planet);
-  }
-
-  getStarship = async (id) => {
-    const starship = await this.getResource(`/starships/${id}/`)
-    return this._transformStarship(starship);
-  }
-
-  getPersonImage = (id) => {
-    return `${this._imageBase}/characters/${id}.jpg`
-  }
-
-  getPlanetImage = (id) => {
-    return `${this._imageBase}/planets/${id}.jpg`
-  }
-
-  getStarshipImage = (id) => {
-    return `${this._imageBase}/starships/${id}.jpg`
-  }
-
-  _extractId(item) {
+  #extractId = (item) => {
     const idRegExp = /\/([0-9]*)\/$/;
     return item.url.match(idRegExp)[1];
-  }
+  };
 
-  _transformPerson = (person) => {
-    return {
-      id: this._extractId(person),
-      name: person.name,
-      gender: person.gender,
-      birthYear: person.birth_year,
-      eyeColor: person.eye_color
-    }
-  }
+  #transformPerson = (person) => ({
+    id: this.#extractId(person),
+    name: person.name,
+    gender: person.gender,
+    birthYear: person.birth_year,
+    eyeColor: person.eye_color
+  });
 
-  _transformPlanet = (planet) => {
-    return {
-      id: this._extractId(planet),
-      name: planet.name,
-      population: planet.population,
-      rotationPeriod: planet.rotation_period,
-      diameter: planet.diameter,
-    }
-  }
+  #transformPlanet = (planet) => ({
+    id: this.#extractId(planet),
+    name: planet.name,
+    population: planet.population,
+    rotationPeriod: planet.rotation_period,
+    diameter: planet.diameter,
+  });
 
-  _transformStarship = (starship) => {
-    return {
-      id: this._extractId(starship),
-      name: starship.name,
-      model: starship.model,
-      manufacturer: starship.manufacturer,
-      costInCredits: starship.cost_in_credits,
-      length: starship.length,
-      crew: starship.crew,
-      passengers: starship.passengers,
-      cargoCapacity: starship.cargo_capacity
-    }
-  }
+  #transformStarship = (starship) => ({
+    id: this.#extractId(starship),
+    name: starship.name,
+    model: starship.model,
+    manufacturer: starship.manufacturer,
+    costInCredits: starship.cost_in_credits,
+    length: starship.length,
+    crew: starship.crew,
+    passengers: starship.passengers,
+    cargoCapacity: starship.cargo_capacity
+  });
 }
