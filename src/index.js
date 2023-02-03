9c516d3d56f34ac2aabb382873cacf096a730e95
Vladislav <flickervladislav@gmail.com>
2023-02-03 19:23:05 +0200

    feat: add finished application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
 
-import { App } from './App';
+import './api/server'
+
+import store from './store'
+import { fetchTodos } from './features/todos/todosSlice'
+
+store.dispatch(fetchTodos())
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
-);
+)
