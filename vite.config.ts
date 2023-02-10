bed3ec7c30e16e760492cd3bc64f0d475655dcd9
Vladislav <flickervladislav@gmail.com>
2023-02-10 12:37:12 +0200

    chore: add ts checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
   test: {
     globals: true,
     environment: 'jsdom',
