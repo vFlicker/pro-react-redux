c09e121241f2656ae858b468d5f27dc47ffd04dc
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-10-22 18:42:55 +0300

    refactor(quiz): replace apiService with fakeApiService for quiz questions

--- quiz/src/app/App.jsx
+++ quiz/src/app/App.jsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header';
 import { QueryResult } from '../components/QueryResult';
 import { Quiz } from '../components/Quiz';
 import { useFetch } from '../hooks/useFetch';
-import { fetchQuestions } from '../services/apiService';
+import { fetchQuestions } from '../services/fakeApiService';
 
 export function App() {
   const { error, data, isLoading } = useFetch(fetchQuestions, []);
